64ef263ee0af47e737d92b112f7180b05aa61628
Cian O'Gralaigh <ihatecian@gmail.com>
2019-06-14 18:53:58 +0100

    improve dynamic ingreident display

--- src/components/Burger/burger.js
+++ src/components/Burger/burger.js
@@ -1,13 +1,21 @@
-import React from "react";
-import styles from "./burger.module.css";
-import { Ingredient } from "./Ingredients/ingredient";
+import React from 'react';
+import styles from './burger.module.css';
+import { Ingredient } from './Ingredients/ingredient';
 
 export const Burger = props => {
-  const transformIngredients = Object.keys(props.ingredients).map(ingKey => {
-    return [...Array(props.ingredients[ingKey])].map((_, i) => {
-      return <Ingredient key={ingKey + i} type={ingKey} />;
-    });
-  });
+  let transformIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <Ingredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients</p>;
+  }
   return (
     <div className={styles.Burger}>
       <Ingredient type="bread-top" />
