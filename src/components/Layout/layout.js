a0509323be0694f1d5018e439bf4fc68046e85b3
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-06-14 15:09:55 +0100

    Add burger related components
    Created BurgerBuilder component
    Created layout CSS module

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Wrapper } from "../Wrapper/wrapper";
+import styles from "./layout.module.css";
 
 export const Layout = props => (
   <Wrapper>
     <div>Toolbar, SideDrawer, BackDrop</div>
-    <main>{props.children}</main>
+    <main className={styles.Content}>{props.children}</main>
   </Wrapper>
 );
---
+++ src/containers/burger-builder/burger-builder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Wrapper } from "../../components/Wrapper/wrapper";
+
+export class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Wrapper>
+    );
+  }
+}
