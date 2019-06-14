cb57201ea751bdd50d48466f5dfa3e341986e647
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-06-14 17:10:31 +0100

    Add state to burger component

--- src/components/Burger/burger.js
+++ src/components/Burger/burger.js
@@ -3,11 +3,15 @@ import styles from "./burger.module.css";
 import { Ingredient } from "./Ingredients/ingredient";
 
 export const Burger = props => {
+  const transformIngredients = Object.keys(props.ingredients).map(ingKey => {
+    return [...Array(props.ingredients[ingKey])].map((_, i) => {
+      return <Ingredient key={ingKey + i} type={ingKey} />;
+    });
+  });
   return (
     <div className={styles.Burger}>
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {transformIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
--- src/containers/burger-builder/burger-builder.js
+++ src/containers/burger-builder/burger-builder.js
@@ -3,10 +3,22 @@ import { Wrapper } from "../../components/Wrapper/wrapper";
 import { Burger } from "../../components/Burger/burger";
 
 export class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meat: 2
+      }
+    };
+  }
+
   render() {
     return (
       <Wrapper>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Wrapper>
     );
