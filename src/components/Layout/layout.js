ccf6956c628e8ec9b602b75f820861af2048cf3a
Cian O'Gralaigh <ihatecian@gmail.com>
2019-06-15 18:12:07 +0100

    Add navbar skeleton

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Wrapper } from "../Wrapper/wrapper";
-import styles from "./layout.module.css";
+import React from 'react';
+import { Wrapper } from '../Wrapper/wrapper';
+import styles from './layout.module.css';
+import { Navbar } from '../navbar/navbar';
 
 export const Layout = props => (
   <Wrapper>
+    <Navbar />
     <div>Toolbar, SideDrawer, BackDrop</div>
     <main className={styles.Content}>{props.children}</main>
   </Wrapper>
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './navbar.module.css';
+
+export const Navbar = props => (
+  <header className={styles.Navbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>
+      <ul>...</ul>
+    </nav>
+  </header>
+);
