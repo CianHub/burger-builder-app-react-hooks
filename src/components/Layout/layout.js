76d7cf350a3fc47b35ae864650a9b05f554694d1
Cian O'Gralaigh <ihatecian@gmail.com>
2019-06-16 19:18:20 +0100

    add backdrop to sidedrawer

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Wrapper } from '../Wrapper/wrapper';
 import styles from './layout.module.css';
 import { Navbar } from '../navbar/navbar';
 import { Sidedrawer } from '../Sidedrawer/sidedrawer';
 
-export const Layout = props => (
-  <Wrapper>
-    <Navbar />
-    <Sidedrawer />
-    <main className={styles.Content}>{props.children}</main>
-  </Wrapper>
-);
+export class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    showSideDrawer: true
+  };
+  sideDrawerClosed = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Wrapper>
+        <Navbar />
+        <Sidedrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosed}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Wrapper>
+    );
+  }
+}
--- src/components/Sidedrawer/sidedrawer.js
+++ src/components/Sidedrawer/sidedrawer.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { Logo } from '../Logo/logo';
 import { NavigationItems } from '../Navigation/navigation';
 import styles from './sidedrawer.module.css';
+import { Wrapper } from '../Wrapper/wrapper';
+import { Backdrop } from '../ui/Backdrop/backdrop';
 
 export const Sidedrawer = props => {
+  let attachedClasses = [styles.Sidedrawer, styles.Close];
+
+  if (props.open) {
+    attachedClasses = [styles.Sidedrawer, styles.Open];
+  }
   return (
-    <div className={styles.Sidedrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Wrapper>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={styles.Sidedrawer}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Wrapper>
   );
 };
