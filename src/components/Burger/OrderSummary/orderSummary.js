3c8a6c1f3cf977a13ba1b684b2336e8dcf120a60
Cian O'Gralaigh <ihatecian@gmail.com>
2019-06-16 19:49:49 +0100

    improve performance with should component update

--- src/components/Burger/OrderSummary/orderSummary.js
+++ src/components/Burger/OrderSummary/orderSummary.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Wrapper } from '../../Wrapper/wrapper';
 import { Button } from '../../ui/button/button';
 
-export const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map((key, index) => (
-    <li key={key + index}>
-      <span style={{ textTransform: 'capitalize' }}>
-        {key}: {props.ingredients[key]}
-      </span>
-    </li>
-  ));
-  return (
-    <Wrapper>
-      <h3>Your Order</h3>
-      <p> A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.totalPrice.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={props.purchaseCancelled} btnType={'Danger'}>
-        CANCEL
-      </Button>
-      <Button clicked={props.purchaseContinued} btnType={'Success'}>
-        CONTINUE
-      </Button>
-    </Wrapper>
-  );
-};
+export class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('OrderSummary Update');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (key, index) => (
+        <li key={key + index}>
+          <span style={{ textTransform: 'capitalize' }}>
+            {key}: {this.props.ingredients[key]}
+          </span>
+        </li>
+      )
+    );
+    return (
+      <Wrapper>
+        <h3>Your Order</h3>
+        <p> A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={this.props.purchaseCancelled} btnType={'Danger'}>
+          CANCEL
+        </Button>
+        <Button clicked={this.props.purchaseContinued} btnType={'Success'}>
+          CONTINUE
+        </Button>
+      </Wrapper>
+    );
+  }
+}
--- src/components/ui/Modal/modal.js
+++ src/components/ui/Modal/modal.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './modal.module.css';
 import { Wrapper } from '../../Wrapper/wrapper';
 import { Backdrop } from '../Backdrop/backdrop';
 
-export const Modal = props => (
-  <Wrapper>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Wrapper>
-);
+export class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Wrapper>
+    );
+  }
+}
