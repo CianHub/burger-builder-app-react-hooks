91dfac21f948b5ff76e419f8a693d4d4da7ace6d
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-15 16:17:18 +0100

    install redux, create store, create initial actions and reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,7 @@
+import * as ACTIONS from "./../actions/actions";
+
+const initialState = { ingredients: null, price: 4 };
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
