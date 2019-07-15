8a29a5c342a173379f14b2e54416bba6c24826ac
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-15 16:35:44 +0100

    Add functionality for add ingreidnet reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,7 +1,27 @@
 import * as ACTIONS from "./../actions/actions";
 
-const initialState = { ingredients: null, price: 4 };
+const initialState = {
+  ingredients: { salad: 0, bacon: 0, cheese: 0, meat: 0 },
+  price: 4
+};
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: {
+          ...this.state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      };
+
+    case ACTIONS.REMOVE_INGREDIENTS:
+      return {};
+
+    default:
+      return {};
+  }
+};
 
 export default reducer;
