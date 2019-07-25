e7c18ed3bd2326b532b46e42fb103252b91a8ab8
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-25 16:57:27 +0100

    refactor burgerIngredient and orderSummary components

--- src/components/Burger/OrderSummary/orderSummary.js
+++ src/components/Burger/OrderSummary/orderSummary.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Wrapper } from "../../Wrapper/wrapper";
 import Button from "../../../components/ui/button/button";
 
-class OrderSummary extends Component {
-  componentWillUpdate() {}
-
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-          {this.props.ingredients[igKey]}
-        </li>
-      );
-    });
-
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return (
-      <Wrapper>
-        <h3>Your Order</h3>
-        <p>Your order contains:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Wrapper>
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        {props.ingredients[igKey]}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <Wrapper>
+      <h3>Your Order</h3>
+      <p>Your order contains:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Wrapper>
+  );
+};
 
 export default OrderSummary;
